32455d00c6a5bd14f3c3f531fb6cc8d89ca82f53
Nikooooh <nicolastrevis@hotmail.com>
2024-05-20 19:55:13 -0300

    automatização python anywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,4 +1,4 @@
-
+from bookstore import views
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as drf_views
@@ -7,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('books.urls')),
     path('api-token-auth/', drf_views.obtain_auth_token, name='api_token_auth'),  
+    path("update_server/", views.update, name="update"),
 ]
